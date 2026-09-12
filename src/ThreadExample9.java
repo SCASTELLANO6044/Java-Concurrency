b9aa3c61eaeb5e3d2cbfe60e0824aa16d2fcec07
Sergio Castellano Betancor <sergiocastellanotrabajo@gmail.com>
2026-09-13 00:31:23 +0200

    :sparkles: Mark a thread as a daemon

---
+++ src/ThreadExample9.java
@@ -0,0 +1,21 @@
+import static java.lang.Thread.sleep;
+
+public class ThreadExample9 {
+    void main() throws InterruptedException {
+        Runnable runnable = ()->{
+            while (true){
+                try {
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                IO.println("Running");
+            }
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.setDaemon(true);
+        thread.start();
+        sleep(3100);
+    }
+}
