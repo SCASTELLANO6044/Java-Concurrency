54274051983577a419a7b92c6332e5ee53e16f3f
Sergio Castellano Betancor <sergiocastellanotrabajo@gmail.com>
2026-09-13 00:44:50 +0200

    :sparkles: Join Threads

---
+++ src/ThreadExample10.java
@@ -0,0 +1,26 @@
+import static java.lang.Thread.sleep;
+
+public class ThreadExample10 {
+    void main(){
+        Runnable runnable = () ->{
+            for (int i = 0; i < 5; i++) {
+                try {
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                IO.println("Running");
+            }
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.setDaemon(true);
+        thread.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
