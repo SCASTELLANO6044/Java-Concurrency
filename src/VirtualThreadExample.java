71ea71601963e70b923009f945f3ba27c7e54f34
Sergio Castellano Betancor <sergiocastellanotrabajo@gmail.com>
2026-09-13 14:57:36 +0200

    :sparkles: Virtual Threads Example

---
+++ src/VirtualThreadExample.java
@@ -0,0 +1,21 @@
+public class VirtualThreadExample {
+    void main(){
+        Runnable runnable = () -> {
+            for (int i = 0; i < 10; i++) {
+                IO.println("Index: "+i);
+            }
+        };
+
+        Thread vThread1 = Thread.ofVirtual().start(runnable);
+
+        Thread vThreadUnstarted = Thread.ofVirtual().unstarted(runnable);
+
+        vThreadUnstarted.start();
+
+        try {
+            vThreadUnstarted.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/VirtualThreadExample2.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class VirtualThreadExample2 {
+    void main(){
+        List<Thread> vThreads = new ArrayList<>();
+
+        int vThreadCount = 100_000;
+
+        for (int i = 0; i < vThreadCount; i++) {
+            int vThreadIndex = i;
+            Thread thread = Thread.ofVirtual().start(()->{
+                int result = 1;
+                for (int j = 0; j < 10; j++) {
+                    result *= (j+1);
+                }
+                IO.println("Result["+vThreadIndex+"]: "+result);
+            });
+            vThreads.add(thread);
+        }
+
+        for (int i = 0; i < vThreads.size(); i++) {
+            try {
+                vThreads.get(i).join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
